b2c6d2c7b2c1ac44b39c9370ee90011670a4c2f0
Gabin <hello@gabinaureche.com>
2017-05-29 21:51:41 +0200

    Add createNode

--- src/__tests__/index.js
+++
@@ -1,9 +0,0 @@
-import test from 'ava';
-import node from '../';
-
-test('should create a block', (t) => {
-    const actual = node();
-    const expected = 'foo';
-
-    t.is(actual, expected);
-});
