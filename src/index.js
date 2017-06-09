045a0ed8319967c89cf12f9a01880d8bdde90b83
Gabin <hello@gabinaureche.com>
2017-06-09 10:23:34 +0200

    Clean

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-export {
-    default as create,
-    createState,
-    createDocument,
-    createBlock,
-    createInline,
-    createText,
-    createMark
-} from './create';
+export { default as create } from './create';
+export * from './create';
 export default from './createHyperscript';
